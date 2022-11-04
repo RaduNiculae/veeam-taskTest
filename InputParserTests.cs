3dfcfab41875e352c65fe9e0d178facb497b688a
Niculae Radu <radu.niculae@guillemot.ro>
2022-11-04 10:30:22 +0200

    initial commit

---
+++ InputParserTests.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using veeam_task;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace veeam_task.Tests
+{
+    [TestClass()]
+    public class InputParserTests
+    {
+        [TestMethod()]
+        public void InputParserTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void parseInputTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
